2d98c489bf339c6052659ea70bb7ff9a7eabe8aa
Illia Kuzmenko <illia.kuzmenko@icloud.com>
2022-02-20 21:08:07 +0200

    Add and update functions usind yield
    Add incidence_matrix, term_permutation_index and term_trigram_index

--- tools/buildindex.py
+++ tools/buildindex.py
@@ -1,48 +1,50 @@
 import re
-import itertools
-import pandas as pd
+from typing import Generator
+
+from nltk import ngrams, stem
 
 
 class BuildIndex:
 
     def __init__(self, files):
-        self.tf = {}
-        self.df = {}
-        self.filenames = files
-        self.file_terms = self.process_files()
-        self.terms_collection = self.terms_collecting()
-        self.incidence_matrix = self.term_file_list()  # incidence_matrix
-        self.file_terms_indices = self.make_indices()
-        self.inverted_index = self.files_posting_list()  # inverted_index
+        self.tf: dict = {}
+        self.df: dict = {}
+        self.filenames: list = files
 
-    def process_files(self) -> dict:
-        """ :param: = [file1, file2, ...]
-            :return: = {filename: [term1, term2]} """
+        self.file_terms: dict = self._process_files()
+        self.vocabulary: list = self._distinct_sorted_terms()
+
+        self.incidence_matrix: Generator[tuple, None, None] = self._term_file_incidence_matrix()
+
+        self.file_posting_list: Generator[tuple, dict, None] = self._coord_index()
+        self.term_posting_list: dict = self._inverted_coord_index()
+
+        self.term_permutation_index: Generator[tuple, None, None] = self._permutation_index()
+        self.term_trigram_index: Generator[tuple, None, None] = self._terms_trigram_index()
+
+    def _process_files(self) -> dict:
+        """ :return: = {filename: [term1, term2]} """
+        stemmer = stem.SnowballStemmer('english')
+        stopwords = open('tools/stopwords.txt').read()
         file_to_terms = {}
         for file in self.filenames:
-            stopwords = open('tools/stopwords.txt').read()
             pattern = re.compile(r'[\W_]+')
             file_to_terms[file] = open(file, 'r', encoding='utf-8').read().lower()
             file_to_terms[file] = pattern.sub(' ', file_to_terms[file])
             re.sub(r'[\W_]+', '', file_to_terms[file])
             file_to_terms[file] = file_to_terms[file].split()
-            file_to_terms[file] = [w for w in file_to_terms[file] if w not in stopwords]
-            # file_to_terms[file] = [stemmer.stem_word(w) for w in file_to_terms[file]]
+            file_to_terms[file] = [_ for _ in file_to_terms[file] if _ not in stopwords]
+            file_to_terms[file] = [stemmer.stem(_) for _ in file_to_terms[file]]
         return file_to_terms
 
-    def terms_collecting(self) -> list:
-        """ :return: list of distinct sorted terms """
-        return sorted(set(list(itertools.chain.from_iterable(self.file_terms.values()))))
+    def _distinct_sorted_terms(self) -> list:
+        """ :return: = ['aterm', 'bterm', 'cterm'] """
+        return sorted(set([_ for __ in self.file_terms.values() for _ in __]))
 
-    def term_file_list(self) -> pd.DataFrame:
-        """ :return: DataFrame with incidence matrix """
-        incidence_matrix = pd.DataFrame(self.terms_collection, columns={'terms_collection'})
-        for file in self.filenames:
-            incidence_matrix[file] = incidence_matrix['terms_collection'].isin(self.file_terms[file])
-        # Count values by file
-        # print(incidence_matrix)
-        # print(incidence_matrix.drop(['terms_collection'], axis=1).apply(pd.Series.value_counts))
-        return incidence_matrix
+    def _term_file_incidence_matrix(self) -> Generator[tuple, list, None]:
+        """ :return: ('term', [0, 0, 1, 0, 0, 1, 0, 0, 1, 0]) """
+        for term in self.vocabulary:
+            yield term, [1 if term in self.file_terms[_] else 0 for _ in self.file_terms]
 
     def index_one_file(self, term_list: dict) -> dict:
         """ :param: = [term1, term2, ...]
@@ -55,32 +57,57 @@ class BuildIndex:
                 file_index[term] = [index]
         return file_index
 
-    def make_indices(self) -> dict:
+    def _coord_index(self) -> Generator[tuple, dict, None]:
         """ :param: = {filename: [term1, term2, ...], ...}
             :return: = {filename: {term: [pos1, pos2, ...]}, ...} """
-        file_terms_indices = {}
-        for filename in self.file_terms.keys():
-            file_terms_indices[filename] = self.index_one_file(self.file_terms[filename])
-        return file_terms_indices
+        for filename in self.filenames:
+            yield filename, self.index_one_file(self.file_terms[filename])
 
-    def files_posting_list(self) -> dict:
+    def _inverted_coord_index(self) -> dict:
         """ :param: = {filename: {term: [pos1, pos2, ...], ... }}
             :return: = {term: {filename: [pos1, pos2]}, ...}, ...} """
         inverted_index = {}
-        file_terms_indices = self.file_terms_indices
-        for filename in file_terms_indices.keys():
+        file_posting_list = {key: value for key, value in self._coord_index()}
+        for filename in file_posting_list.keys():
             self.tf[filename] = {}
-            for term in file_terms_indices[filename].keys():
-                self.tf[filename][term] = len(file_terms_indices[filename][term])
+            for term in file_posting_list[filename].keys():
+                self.tf[filename][term] = len(file_posting_list[filename][term])
                 if term in self.df.keys():
                     self.df[term] += 1
                 else:
                     self.df[term] = 1
                 if term in inverted_index.keys():
                     if filename in inverted_index[term].keys():
-                        inverted_index[term][filename].append(file_terms_indices[filename][term][:])
+                        inverted_index[term][filename].append(file_posting_list[filename][term][:])
                     else:
-                        inverted_index[term][filename] = file_terms_indices[filename][term]
+                        inverted_index[term][filename] = file_posting_list[filename][term]
                 else:
-                    inverted_index[term] = {filename: file_terms_indices[filename][term]}
+                    inverted_index[term] = {filename: file_posting_list[filename][term]}
         return inverted_index
+
+    def _permutation_index(self) -> Generator[tuple, list, None]:
+        """ :return: permutation index for terms """
+        for term in list(self.vocabulary):
+            jok_term = term + '$'
+            yield term, [jok_term[_:] + jok_term[:_] for _ in range(len(jok_term))]
+
+    def _terms_trigram_index(self) -> Generator[tuple, list, None]:
+        """ :return: trigrams for terms """
+        for term in self.vocabulary:
+            yield term, list(ngrams(term, 3))
+
+
+if __name__ == '__main__':
+    filenames = [
+        'corpus/The-Romance-of-a-Sho-Amy-Levy.txt',
+        'corpus/An-English-Grammar-J-W-Sewell.txt',
+        'corpus/Cunningham-s-manual-D-J-Daniel-J.txt',
+        'corpus/Famous-Composers-and-Various.txt',
+        'corpus/On-Sunset-Highways-Thomas-D-Murph.txt',
+        'corpus/Passages-from-the-Li-Charles-Babbage.txt',
+        'corpus/Secret-Adversary-Agatha-Christie.txt',
+        'corpus/Sir-Edwin-Landseer-Frederick-G-St.txt',
+        'corpus/Telescopic-Work-for-William-F-Denn.txt',
+        'corpus/The-Letters-of-a-Por-Marianna-Alcofo.txt'
+    ]
+    index = BuildIndex(filenames)
